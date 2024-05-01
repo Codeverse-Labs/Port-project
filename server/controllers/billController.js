bb12506388b722228b05847b99cfa3755b4cff63
thenuka99 <janithherath2@gmail.com>
2024-05-01 19:57:58 +0530

    refactor : update into new db

--- server/controllers/billController.js
+++ server/controllers/billController.js
@@ -2,6 +2,11 @@ const xlsx = require('xlsx');
 const mysql = require('mysql');
 const db = require('../config/query');
 
+const { format } = require('date-fns');
+
+// Get today's date in 'YYYY-MM-DD' format
+const today_date = format(new Date(), 'yyyy-MM-dd');
+
 // Function to handle bill upload
 exports.uploadBill = (req, res) => {
   // Check if file is provided
@@ -20,62 +25,62 @@ exports.uploadBill = (req, res) => {
   (Mobile, Conn, Divdat, GPRS, Data, MMSRnt, OUG, MMS, OutSMS, IncSMS, Roam, IDD, IDDReb, Pack, VPN, RegFee, Nor, VdCall, VdIDD, Wap, SubRb, Mtune, Msub, PMsubs, Pmail, USDSMS, USSD, CLI, Val50, VRC, Info, News, ECH, ESer, Devices, Services, DBR, STK, Fax, BLB, IDT, IDTReb, VAT, NBT, LCF, TLE, SSCL_TAX, Total, Month, Year) 
   VALUES ?`; // Modify this query according to your table structure
 
-const currentDate = new Date();
-const currentMonth = currentDate.getMonth() + 1; // Adding 1 because getMonth() returns zero-based index
-const currentYear = currentDate.getFullYear();
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth() + 1; // Adding 1 because getMonth() returns zero-based index
+  const currentYear = currentDate.getFullYear();
 
-const values = data.map(row => [
-row.Mobile,
-row.Conn,
-row.Div,
-row.GPRS,
-row.Data,
-row['MMS Rnt'],
-row.OUG,
-row.MMS,
-row['Out SMS'],
-row['Inc SMS'],
-row.Roam,
-row.IDD,
-row['IDD Reb'],
-row.Pack,
-row.VPN,
-row['Reg Fee'],
-row.Nor,
-row['Vd Call'],
-row['Vd IDD'],
-row.Wap,
-row['Sub Rb'],
-row['M tune'],
-row.Msub,
-row['PM subs'],
-row.Pmail,
-row['USD SMS'],
-row.USSD,
-row.CLI,
-row.Val50,
-row.VRC,
-row.Info,
-row.News,
-row.ECH,
-row['E Ser'],
-row.Devices,
-row.Services,
-row.DBR,
-row.STK,
-row.Fax,
-row.BLB,
-row.IDT,
-row['IDT Reb'],
-row.VAT,
-row.NBT,
-row.LCF,
-row.TLE,
-row.SSCL_TAX,
-row.Total,
-currentMonth,
-currentYear
-]); // Modify this according to your column names
+  const values = data.map(row => [
+    row.Mobile,
+    row.Conn,
+    row.Div,
+    row.GPRS,
+    row.Data,
+    row['MMS Rnt'],
+    row.OUG,
+    row.MMS,
+    row['Out SMS'],
+    row['Inc SMS'],
+    row.Roam,
+    row.IDD,
+    row['IDD Reb'],
+    row.Pack,
+    row.VPN,
+    row['Reg Fee'],
+    row.Nor,
+    row['Vd Call'],
+    row['Vd IDD'],
+    row.Wap,
+    row['Sub Rb'],
+    row['M tune'],
+    row.Msub,
+    row['PM subs'],
+    row.Pmail,
+    row['USD SMS'],
+    row.USSD,
+    row.CLI,
+    row.Val50,
+    row.VRC,
+    row.Info,
+    row.News,
+    row.ECH,
+    row['E Ser'],
+    row.Devices,
+    row.Services,
+    row.DBR,
+    row.STK,
+    row.Fax,
+    row.BLB,
+    row.IDT,
+    row['IDT Reb'],
+    row.VAT,
+    row.NBT,
+    row.LCF,
+    row.TLE,
+    row.SSCL_TAX,
+    row.Total,
+    currentMonth,
+    currentYear
+  ]); // Modify this according to your column names
 
   db.getConnection((err, connection) => {
     if (err) {
@@ -101,18 +106,21 @@ exports.getMonthlyBills = (req, res) => {
   const { month, year } = req.query; // Assuming you're passing month and year as query parameters
 
   const sql = `
-  SELECT bills.*, users.*
-  FROM bills
-  JOIN users ON bills.Mobile = users.id
-  WHERE bills.Month = ? AND bills.Year = ?;
+  SELECT u.*, um.*, b.*
+  FROM users u
+  JOIN user_mobile um ON um.UserId = u.Id
+  JOIN bills b ON b.Mobile = um.MobileNumber
+  WHERE um.GivenFrom < ?
+  AND (um.GivenUntill IS NULL OR um.GivenUntill > ?)
+  AND b.Month = ? AND b.Year = ?;
   `;
 
-  db.query(sql, [month, year], (err, results) => {
+  db.query(sql, [today_date,today_date,month, year], (err, results) => {
     if (err) {
       console.error('Error fetching bill data:', err);
-      return res.status(500).json({ error: 'Database error' });
+      return res.status(500).json({ error: 'Database error' + err });
     }
-    
+
     res.json(results);
   });
 };
@@ -121,18 +129,21 @@ exports.getDptBills = (req, res) => {
   const { month, year, Dpt } = req.query; // Assuming you're passing month and year as query parameters
 
   const sql = `
-  SELECT bills.*, users.*
-  FROM bills
-  JOIN users ON bills.Mobile = users.id
-  WHERE bills.Month = ? AND bills.Year = ? AND users.Dpt = ?;
+  SELECT u.*, um.*, b.*
+  FROM users u
+  JOIN user_mobile um ON um.UserId = u.Id
+  JOIN bills b ON b.Mobile = um.MobileNumber
+  WHERE um.GivenFrom < ?
+  AND (um.GivenUntill IS NULL OR um.GivenUntill > ?)
+  AND b.Month = ? AND b.Year = ? AND u.Dpt = ?;
   `;
 
-  db.query(sql, [month, year, Dpt], (err, results) => {
+  db.query(sql, [today_date,today_date,month, year, Dpt], (err, results) => {
     if (err) {
       console.error('Error fetching bill data:', err);
       return res.status(500).json({ error: 'Database error' });
     }
-    
+
     res.json(results);
   });
 };
