41f24f581a85d99a8d3bda396c2ed86cb80152d3
vidithw20 <vidithw99@gmail.com>
2024-05-07 19:20:44 +0530

    Minor changes

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -2,18 +2,55 @@ import React from 'react';
 import { useParams, useNavigate, useLocation } from 'react-router-dom';
 
 function Sidebar() {
-    const location = useLocation();
-    const navigate = useNavigate();
+  const location = useLocation();
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col justify-items-center min-w-[300px] bg-slate-100 min-h-[100vh] items-center align-middle">
-      <button className={`btn p-8`} onClick ={() => navigate(`/master-bill`)}>
-        <p className={`text-xl ${location.pathname === "/master-bill" ? 'border-b border-black' : ''}`}>Master Bill</p>
+      <button className={`btn p-8`} onClick={() => navigate(`/master-bill`)}>
+        <p
+          className={`text-xl ${
+            location.pathname === '/master-bill' ? 'border-b border-black' : ''
+          }`}
+        >
+          Master Bill
+        </p>
       </button>
-      <button className="btn p-8" onClick ={() => navigate(`/d-summary`)}>
-        <p className={`text-xl ${location.pathname === "/d-summary" ? 'border-b border-black' : ''}`}>Division Summary</p>
+      <button className="btn p-8" onClick={() => navigate(`/d-summary`)}>
+        <p
+          className={`text-xl ${
+            location.pathname === '/d-summary' ? 'border-b border-black' : ''
+          }`}
+        >
+          Division Summary
+        </p>
       </button>
       <button className="btn p-8">
-        <p className={`text-xl ${location.pathname === "/deduction" ? 'border-b border-black' : ''}`}>Deduction</p>
+        <p
+          className={`text-xl ${
+            location.pathname === '/deduction' ? 'border-b border-black' : ''
+          }`}
+        >
+          Deduction
+        </p>
+      </button>
+
+      <button className="btn p-8" onClick={() => navigate(`/departments`)}>
+        <p
+          className={`text-xl ${
+            location.pathname === '/departments' ? 'border-b border-black' : ''
+          }`}
+        >
+          Departments
+        </p>
+      </button>
+      <button className="btn p-8" onClick={() => navigate(`/users`)}>
+        <p
+          className={`text-xl ${
+            location.pathname === '/users' ? 'border-b border-black' : ''
+          }`}
+        >
+          Users
+        </p>
       </button>
     </div>
   );
