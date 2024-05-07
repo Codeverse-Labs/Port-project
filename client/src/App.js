16e2f6c901621a23b94505c3eebfd4f38e642830
vidithw20 <vidithw99@gmail.com>
2024-05-07 19:18:18 +0530

    Added react icons and toastify

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,22 @@ import FormPage from './pages/MasterBillPage/FormPage';
 import MasterBillPage from './pages/MasterBillPage/MasterBillPage';
 import LoginPage from './pages/auth/LoginPage';
 import DSummaryPage from './pages/DivisionSummary/DSummaryPage';
+import DepartmentsPage from './pages/Departments/DepartmentsPage';
+import UsersPage from './pages/Users/UsersPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Example of how to define a router path */} 
-        <Route path="/login" element={<LoginPage/>}/> 
-        <Route path="/" element={<Navigate to={"/login"} replace />}/>
-        <Route path="/dashboard" element={<DashboardPage/>}/> 
-        <Route exact path="/master-bill" element={<FormPage/>}/> 
-        <Route path="/master-bill-sheet" element={<MasterBillPage/>}/> 
-        <Route path="/d-summary" element={<DSummaryPage/>}/> 
+        {/* Example of how to define a router path */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<Navigate to={'/login'} replace />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route exact path="/master-bill" element={<FormPage />} />
+        <Route path="/master-bill-sheet" element={<MasterBillPage />} />
+        <Route path="/d-summary" element={<DSummaryPage />} />
+        <Route path="/departments" element={<DepartmentsPage />} />
+        <Route exact path="/users" element={<UsersPage />} />
       </Routes>
     </BrowserRouter>
   );
