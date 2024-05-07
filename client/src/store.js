bcef9e57cc0e2c205d76d8600c7e333143caa1a8
vidithw20 <vidithw99@gmail.com>
2024-05-07 19:20:12 +0530

    Updated store

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,7 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { userApi } from './services/userService.js';
 import { billApi } from './services/billService.js';
+import { departmentApi } from './services/departmentService.js';
 
 // initial state of the store
 const initialState = {
@@ -11,6 +12,7 @@ const initialState = {
 const combinedReducer = combineReducers({
   [userApi.reducerPath]: userApi.reducer,
   [billApi.reducerPath]: billApi.reducer,
+  [departmentApi.reducerPath]: departmentApi.reducer,
 });
 
 // Create the Redux store with the root reducer
@@ -21,6 +23,7 @@ const store = configureStore({
     getDefaultMiddleware()
       .concat(userApi.middleware)
       .concat(billApi.middleware)
+      .concat(departmentApi.middleware)
 });
 
 export default store;
