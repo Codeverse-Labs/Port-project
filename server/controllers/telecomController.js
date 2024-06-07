07d72003770a96e8e70d3000eb75392f9457cbbd
thenuka99 <janithherath2@gmail.com>
2024-06-07 20:18:49 +0530

    feat : telecom apis

---
+++ server/controllers/telecomController.js
@@ -0,0 +1,47 @@
+const db = require('../config/query');
+
+exports.create = (req, res) => {  
+  const sql = 'INSERT INTO telecom (MobileNumber) VALUES (?)';
+  db.query(sql, [req.body.mobileNumber], (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    } else {
+      res.status(201).json("Created successfully");
+    }
+  });
+};
+
+exports.getAll = (req, res) => {
+  db.query('SELECT * FROM telecom', (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    } else {
+      res.status(200).json(results);
+    }
+  });
+};
+
+exports.updateById = (req, res) => {
+  const sql = 'UPDATE telecom SET MobileNumber = ? WHERE Id = ?';
+  db.query(sql, [
+    req.body.mobileNumber,
+    req.params.id
+  ], (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    } else {
+      res.status(200).json("Updated successfully");
+    }
+  });
+};
+
+exports.deleteById = (req, res) => {
+  const sql = 'DELETE FROM telecom WHERE Id = ?';
+  db.query(sql, [req.params.id], (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    } else {
+      res.status(200).json("Deleted successfully");
+    }
+  });
+};
